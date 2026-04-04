1a53654ade6139fa8cc021d01a2e6f866ca9a758
Dawit Haile <davehaile44@gmail.com>
2026-04-04 09:40:25 +0300

    Refactor CORS and CSRF settings to use environment variables for allowed origins

--- back/pharmacy_erp/pharmacy_erp/settings.py
+++ back/pharmacy_erp/pharmacy_erp/settings.py
@@ -140,11 +140,5 @@ MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
 CORS_ALLOW_ALL_ORIGINS = False
 CORS_ALLOW_CREDENTIALS = True
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:5173",
-    "http://127.0.0.1:5173",
-]
-CSRF_TRUSTED_ORIGINS = [
-    "http://localhost:5173",
-    "http://127.0.0.1:5173",
-]
+CORS_ALLOWED_ORIGINS = os.getenv('CSRF_TRUSTED_ORIGINS', '').split(',')
+CSRF_TRUSTED_ORIGINS = os.getenv('CSRF_TRUSTED_ORIGINS', '').split(',')
